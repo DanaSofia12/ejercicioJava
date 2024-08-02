a96248c66c611a86ca323baa83279bf662c5ad99
amtriana62 <amtriana62@misena.edu.co>
2024-08-02 13:57:16 -0500

    modificado tildes

--- Ejercicio1/src/principal/principal.java
+++ Ejercicio1/src/principal/principal.java
@@ -17,12 +17,14 @@ public class principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
+        /*leer tildes con "ISO-8859-1" o cambiar de clase a BufferedReader*/
+        Scanner teclado = new Scanner(System.in,"ISO-8859-1"); 
         Compra c = new Compra();
         System.out.println("BIENVENIDOS A LA EMPRESA DE HIELO SECO:)");
         System.out.println("A continuacion escribe tu nombre: ");
         c.setNombreCliente(teclado.nextLine());
-        System.out.println("Ahora escribe el numero de tipo de cliente que eres ");
+        /*Cambiar propiedades del proyecto a Windows 1252-clic derecho sobre el proyecto*/
+        System.out.println("Ahora escribe el número de tipo de cliente que eres ");
         c.setTipoCliente(Integer.parseInt(teclado.nextLine()));
         System.out.println("Por ultimo, digita el numero de hojas que compraste: ");
         c.setCantidadHojas(Integer.parseInt(teclado.nextLine()));
